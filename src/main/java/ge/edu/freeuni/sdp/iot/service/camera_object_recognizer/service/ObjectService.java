29d3cf1d8b23f7581607cd5bf7b49359861fa855
misho1995 <mnems13@freeuni.edu.ge>
2016-06-25 17:13:41 +0400

    Add update object by id.

--- src/main/java/ge/edu/freeuni/sdp/iot/service/camera_object_recognizer/service/ObjectService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/service/camera_object_recognizer/service/ObjectService.java
@@ -6,6 +6,7 @@ import ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.data.RepositoryFa
 import ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.model.ObjectEntity;
 import ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.proxy.ProxyFactory;
 
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -39,7 +40,7 @@ public class ObjectService {
     }
 
     @POST
-    public ObjectDo addObject(@PathParam("house_id") String houseId, ObjectDo obj) throws StorageException {
+    public ObjectDo addObject(@PathParam("house_id") String houseId, @NotNull ObjectDo obj) throws StorageException {
         if (!houseExists(houseId) || obj.getType() == null)
             throw new NotFoundException();
         obj.setId(UUID.randomUUID().toString());
@@ -59,6 +60,18 @@ public class ObjectService {
         if (!houseExists(houseId) || entity == null)
             throw new NotFoundException();
         return entity.toDo();
+    }
+
+    @PUT
+    @Path("{object_id}")
+    public ObjectDo updateObjectById(@PathParam("house_id") String houseId,
+                                     @PathParam("object_id") String objectId,
+                                     @NotNull ObjectDo obj) throws StorageException {
+        ObjectDo objectDo = getObjectById(houseId, objectId);
+        // all errors are handled in get object by id
+        objectDo.setType(obj.getType());
+        getRepository().insertOrUpdate(ObjectEntity.fromDo(objectDo));
+        return objectDo;
     }
 
     private boolean houseExists(String id) {
