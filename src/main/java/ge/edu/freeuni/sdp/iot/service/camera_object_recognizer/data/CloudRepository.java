ff7c9e0532f067b095b3ba3e1cfabdb885526cd6
Alexandr Azizyan <alex.r.azizian@gmail.com>
2016-06-25 16:23:51 +0400

    Implement CloudRepository

--- src/main/java/ge/edu/freeuni/sdp/iot/service/camera_object_recognizer/data/CloudRepository.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/service/camera_object_recognizer/data/CloudRepository.java
@@ -2,7 +2,10 @@ package ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.data;
 
 import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.table.CloudTable;
+import com.microsoft.azure.storage.table.TableOperation;
+import com.microsoft.azure.storage.table.TableQuery;
 import ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.model.ObjectEntity;
+import ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.model.ObjectEntityId;
 
 public class CloudRepository implements Repository {
 
@@ -13,23 +16,33 @@ public class CloudRepository implements Repository {
     }
 
     @Override
-    public void insertOrUpdate(ObjectEntity task) throws StorageException {
-
+    public void insertOrUpdate(ObjectEntity object) throws StorageException {
+        table.execute(TableOperation.insertOrReplace(object));
     }
 
 
     @Override
     public ObjectEntity delete(String id) throws StorageException {
-        return null;
+        ObjectEntity object = find(id);
+        if (object != null) {
+            TableOperation operation = TableOperation.delete(object);
+            table.execute(operation);
+        }
+
+        return object;
     }
 
     @Override
     public ObjectEntity find(String id) throws StorageException {
-        return null;
+        ObjectEntityId objectEntityId = new ObjectEntityId(id);
+        TableOperation operation = TableOperation.retrieve(
+                objectEntityId.getPartitionKey(), objectEntityId.getRowKey(), ObjectEntity.class);
+
+        return table.execute(operation).getResultAsType();
     }
 
     @Override
     public Iterable<ObjectEntity> getAll() {
-        return null;
+        return table.execute(TableQuery.from(ObjectEntity.class));
     }
 }
