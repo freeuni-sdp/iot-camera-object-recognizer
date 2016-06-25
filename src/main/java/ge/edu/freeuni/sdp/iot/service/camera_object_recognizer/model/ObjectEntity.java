0d57ca43db3238b8cdd99fad317091ff8ade6211
Alexandr Azizyan <alex.r.azizian@gmail.com>
2016-06-25 16:09:46 +0400

    Add object entity

--- src/main/java/ge/edu/freeuni/sdp/iot/service/camera_object_recognizer/model/ObjectEntity.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/service/camera_object_recognizer/model/ObjectEntity.java
@@ -1,7 +1,36 @@
 package ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.model;
 
-/**
- * Created by alex on 6/25/16.
- */
-public class ObjectEntity {
+import com.microsoft.azure.storage.table.TableServiceEntity;
+import ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.service.ObjectDo;
+
+public class ObjectEntity extends TableServiceEntity {
+
+    private String type;
+
+    public ObjectEntity() {
+    }
+
+    public ObjectEntity(ObjectDo objectDo) {
+        ObjectEntityId entityId = new ObjectEntityId(objectDo.getId());
+        this.partitionKey = entityId.getPartitionKey();
+        this.rowKey = entityId.getRowKey();
+        this.type = objectDo.getType();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ObjectDo toDo() {
+        String id = new ObjectEntityId(partitionKey, rowKey).getId();
+        return new ObjectDo(id, type);
+    }
+
+    public static ObjectEntity fromDo(ObjectDo objectDo) {
+        return new ObjectEntity(objectDo);
+    }
 }
--- src/main/java/ge/edu/freeuni/sdp/iot/service/camera_object_recognizer/model/ObjectEntityId.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/service/camera_object_recognizer/model/ObjectEntityId.java
@@ -1,7 +1,30 @@
 package ge.edu.freeuni.sdp.iot.service.camera_object_recognizer.model;
 
-/**
- * Created by alex on 6/25/16.
- */
 public class ObjectEntityId {
+
+    private String partitionKey;
+
+    private String rowKey;
+
+    public ObjectEntityId(String id) {
+        this.partitionKey = id.substring(0, 3);
+        this.rowKey = id.substring(3);
+    }
+
+    public ObjectEntityId(String partitionKey, String rowKey) {
+        this.partitionKey = partitionKey;
+        this.rowKey = rowKey;
+    }
+
+    public String getPartitionKey() {
+        return partitionKey;
+    }
+
+    public String getRowKey() {
+        return rowKey;
+    }
+
+    public String getId() {
+        return partitionKey + rowKey;
+    }
 }
